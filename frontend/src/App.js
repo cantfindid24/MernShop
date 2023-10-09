62393f6dfb3cccf32eeb0c0e83e74a484fef4c06
cantfindid24 <106964984+cantfindid24@users.noreply.github.com>
2023-10-09 22:29:13 +0530

    show list of products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,37 @@
+import data from './data.js';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <a href="/">MernShop</a>
       </header>
-      <main>list of Products</main>
+      <main>
+        <div className="product-nav">
+          <h1>Featured Products</h1>
+          <a href="/products/top-rated">Top Rated</a>
+          <a href="/products/best-selling">Best Selling</a>
+          <a href="/products/latest-products">Latest Products</a>
+        </div>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.name}>
+              <a href={`/product/${product.name}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.name}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
+      <footer></footer>
     </div>
   );
 }
