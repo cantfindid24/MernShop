2d3baeac16157d718314bc7025e17fa9a18b34de
cantfindid24 <106964984+cantfindid24@users.noreply.github.com>
2023-10-09 20:30:33 +0530

    Initialise Repository

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">MernShop</a>
+      </header>
+      <main>list of Products</main>
+    </div>
+  );
+}
+
+export default App;
