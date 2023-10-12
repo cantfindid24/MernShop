6e382cc158340e04e3b4a7a97bbcf8c9265d9337
cantfindid24 <106964984+cantfindid24@users.noreply.github.com>
2023-10-12 16:31:09 +0530

    Fetch Products from backend

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,7 +1,16 @@
-import React from 'react';
-import data from '../data';
+import React, { useEffect, useState } from 'react';
+// import data from '../data';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 export default function HomeScreen() {
+  const [productsData, setProductsData] = useState([]);
+  useEffect(() => {
+    const fetchProductsData = async () => {
+      const result = await axios.get('/products/api');
+      setProductsData(result.data);
+    };
+    fetchProductsData();
+  }, []);
   return (
     <div>
       <div className="product-nav">
@@ -11,7 +20,7 @@ export default function HomeScreen() {
         <Link to={'/products/latest-products'}>Latest Products</Link>
       </div>
       <div className="products">
-        {data.products.map((product) => (
+        {productsData.map((product) => (
           <div className="product" key={product.name}>
             <Link to={`/product/${product.name}`}>
               <img src={product.image} alt={product.name} />
